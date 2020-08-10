6214a6db1ebf6b71e8883c0ac9417db512688686
menubboi <69341077+menubboi@users.noreply.github.com>
2020-08-10 16:31:46 +0530

    fclone trial

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://downloads.rclone.org/v1.52.3/rclone-v1.52.3-linux-amd64.zip
-unzip -q rclone-v1.52.3-linux-amd64.zip
-export PATH=$PWD/rclone-v1.52.3-linux-amd64:$PATH
+wget -q https://github.com/mawaya/rclone/releases/download/fclone-v0.3.1/fclone-v0.3.1-linux-amd64.zip
+unzip -q fclone-v0.3.1-linux-amd64.zip
+export PATH=$PWD/fclone-v0.3.1-linux-amd64.zip:$PATH
+
